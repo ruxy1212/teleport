652a1d7a9df1d1719c00058f3757195ce1c02411
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2024-06-30 22:59:40 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,37 +3,20 @@
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Dotenv\Dotenv;
 
 require '../vendor/autoload.php';
 require '../src/helpers.php';
 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 $app = AppFactory::create();
-// $app->get('/ss', function (Request $request, Response $response, array $args){
-//     $response->getBody()->write('Hello, worlsod!');
-//     return $response;
-// });
+
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $routeDefinitions = require '../src/routes.php';
 $routeDefinitions($app);
 
-// Run the app
 $app->run();
 
-
-// $app->get('/s', function (Request $request, Response $response) {
-//     $response->getBody()->write('Hello, world!');
-//     return $response;
-// });
-// // Add routing middleware
-// $app->addRoutingMiddleware();
-
-// // Add error handling middleware
-// $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-// // Include the routes
-// require '../src/routes.php';
-
-// Run the app
-// $app->run();
 ?>
